cdc7216aa0b0f551524f33bda4444a56971d4805
Caroline <cal31391@icloud.com>
2018-02-02 16:53:34 -0500

    fixed SearchComponent spec

--- src/app/ui/session/global-session-list/search/search.component.spec.ts
+++ src/app/ui/session/global-session-list/search/search.component.spec.ts
@@ -3,21 +3,21 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
-import { SearchComponent } from './search.component';
+import {GlobalSessionSearchComponent} from './search.component';
 
 describe('SearchComponent', () => {
-  let component: SearchComponent;
-  let fixture: ComponentFixture<SearchComponent>;
+  let component: GlobalSessionSearchComponent;
+  let fixture: ComponentFixture<GlobalSessionSearchComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SearchComponent ]
+      declarations: [ GlobalSessionSearchComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SearchComponent);
+    fixture = TestBed.createComponent(GlobalSessionSearchComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
