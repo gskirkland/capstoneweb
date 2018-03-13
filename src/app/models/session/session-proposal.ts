4ac4fa82e157bd9717fd8d9e0df8a1bfe633edde
Caroline Lee <clee5@pstcc.edu>
2018-03-13 04:06:07 -0400

    removed placeholder and added logic for populating session data by time and date

--- src/app/models/session/session-proposal.ts
+++ src/app/models/session/session-proposal.ts
@@ -18,6 +18,7 @@ export class SessionProposal {
     Tags: Tag[];
     Room: string;
     StartTime: Date;
+    SpeakerName: string;
 
     constructor() {
         this.Type = SessionType.Standard;
--- src/app/ui/calendar/calendar.component.ts
+++ src/app/ui/calendar/calendar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import * as moment from 'moment';
 import { SessionProposal } from './../../models/session/session-proposal';
 import { SessionService } from './../../services/session.service';
 
@@ -14,21 +15,43 @@ import { SessionService } from './../../services/session.service';
 export class CalendarComponent implements OnInit {
 
     sessions: SessionProposal[];
-    searchTitle: string;
-    searchSpeaker: string;
-    searchAbstract: string;
-    filterTrack: string;
-    timeSlots: string[];
-    timeSlot: string;
+    calendar_obj: {};
 
     constructor(private sessionService:SessionService, private router: Router){}
 
     ngOnInit() {
-        this.sessionService.getAllSessionProposals()
+        this.sessionService.getAllAcceptedSessionProposals()
             .then(sessions => {
                 this.sessions = sessions;
-            });
 
-        this.timeSlots = ['10A', '11A', '12P', '1P', '2P', '3P', '4P', '5P'];
+                let calendar_obj = <any>{};
+                this.sessions.forEach(function(item) {
+                    let session_obj = <any>{};
+                    session_obj.Title = item.Title;
+                    session_obj.Abstract = item.Abstract;
+                    session_obj.SpeakerName = item.SpeakerName;
+                    session_obj.Track = item.Track;
+                    session_obj.Room = item.Room;
+
+                    let start_time = moment(item.StartTime);
+
+                    let day = start_time.date();
+
+                    let time = moment(start_time.hour() + ':' + start_time.minutes(), 'HH:mm').format('hh:mm a');
+
+                    if (!(day in calendar_obj)) {
+                        calendar_obj[day] = {
+                            time_blocks: {}
+                        };
+                    }
+
+                    if (!(time in calendar_obj[day].time_blocks)) {
+                        calendar_obj[day].time_blocks[time] = [];
+                    }
+
+                    calendar_obj[day].time_blocks[time].push(session_obj);
+                });
+                this.calendar_obj = calendar_obj;
+            });
     }
 }
